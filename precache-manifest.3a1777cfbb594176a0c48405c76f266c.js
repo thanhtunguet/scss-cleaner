2ad6819292860ded56da2477016cdbb81b93d1cc
thanhtunguet <ht@thanhtunguet.info>
2020-11-19 13:41:22 +0000

    Deploying to gh-pages from  @ 1a4c0de77557b9b9293de7dcadfacaac9c0f2daf 🚀

---
+++ precache-manifest.3a1777cfbb594176a0c48405c76f266c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c050f95c2904c56b007bc77191ccd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18476d54c4c44871a126",
+    "url": "/static/css/2.2be137b4.chunk.css"
+  },
+  {
+    "revision": "63f4840da3f02e773aa3",
+    "url": "/static/css/main.7aac3462.chunk.css"
+  },
+  {
+    "revision": "18476d54c4c44871a126",
+    "url": "/static/js/2.79ccdeb2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.79ccdeb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f4840da3f02e773aa3",
+    "url": "/static/js/main.f79555b1.chunk.js"
+  },
+  {
+    "revision": "d061362977b74b3b1dd6",
+    "url": "/static/js/runtime-main.20b96635.js"
+  }
+]);
